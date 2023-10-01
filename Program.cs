d776e870f38cb0f4c98acd43f312bd859232c80b
JS <Yla45@yandex.ru>
2023-10-01 22:56:18 +0300

    Code addad

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string [] array = {"left", "see", "^_^", "Julia", "you"};
+string [] array2 = new string [array.Length];
+
+string [] find3CharStrings ()
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                array2[count] = array[i];
+                count ++;
+            }
+        }
+        return array2;
+    }
+
+void printArray()
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+
+void printArray2()
+    {
+        for (int i = 0; i < array2.Length; i++)
+        {
+            Console.WriteLine(array2[i]);
+        }
+    }
+
+find3CharStrings();
+printArray();
+Console.WriteLine();
+printArray2();
+
+
+        
